934ba2fb9a8f71aa42c333137b20d7bb4b6f77ce
Adil Chaka <achaka@stevens.edu>
2024-04-11 16:41:37 -0400

    Adding Model Viewer Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './HomePage';
 import CameraComponent from './CameraComponent';  // Assuming CameraComponent is its own page now
 import UserTutorialPage from './UserTutorialPage';
+import ModelViewer from './ModelViewer';
 
 function App() {
   return (
@@ -19,12 +20,16 @@ function App() {
             <li>
               <Link to="/tutorial">User Tutorial</Link>
             </li>
+            <li>
+              <Link to="/ModelViewer">Model Viewer</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/camera" element={<CameraComponent />} />
           <Route path="/tutorial" element={<UserTutorialPage />} />
+          <Route path="/ModelViewer" element={<ModelViewer />} />
         </Routes>
       </div>
     </Router>
---
+++ src/ModelViewer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ModelViewer() {
+  return (
+    <div>
+      <h1>Model Viewer</h1>
+      <p>This is the Model Viewer.</p>
+    </div>
+  );
+}
+
+export default ModelViewer;
