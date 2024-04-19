5a248058544c0579dffc43736075f14b1790efd1
Adil Chaka <achaka@stevens.edu>
2024-04-19 15:22:01 -0400

    Updating User Tutorial page

--- src/UserTutorialPage.js
+++ src/UserTutorialPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import photoUploadPageImage from './photoUploadPageImage'; //Import image for the photo upload
-import shirtImage from './shirt.jpg'; //Import image for the shirt
-import pantsImage from '/.pants.jpg'; //Import image for the pants
-import frontImagephoto from './front_photo.jpg'; //Import image for the front photo 
-import backImagephoto from './back_photo.jpg'; //Import image for the back photo 
-import segmentImage from './segment.jpg'; //Import image for the segment step
+import photoUploadPageImage from './UserTutorialImages/photoUploadPageImage.jpg'; //Import image for the photo upload
+import shirtImage from './UserTutorialImages/shirt.jpg'; //Import image for the shirt
+import pantsImage from './UserTutorialImages/pants.jpg'; //Import image for the pants
+import frontPhotoImage from './UserTutorialImages/front_photo.jpg'; //Import image for the front photo 
+import backPhotoImage from './UserTutorialImages/back_photo.jpg'; //Import image for the back photo 
+import SegmentImage from './UserTutorialImages/segment.jpg'; //Import image for the segment step
 
 
 function UserTutorialPage() {
