839108cecd32ca9af66c3cb34c1c92499fe67a7e
Adil Chaka <achaka@stevens.edu>
2024-04-25 19:46:11 -0400

    Updating User Tutorial and Camera pages

--- src/UserTutorialPage.js
+++ src/UserTutorialPage.js
@@ -6,7 +6,12 @@ import pantsImage from './UserTutorialImages/pants.jpg'; //Import image for the 
 import frontPhotoImage from './UserTutorialImages/front_photo.jpg'; //Import image for the front photo 
 import backPhotoImage from './UserTutorialImages/back_photo.jpg'; //Import image for the back photo 
 import SegmentImage from './UserTutorialImages/segment.jpg'; //Import image for the segment step
-
+import Step1 from './UserTutorialImages/NewImages/Step1.png';
+import Step2 from './UserTutorialImages/NewImages/Step2.png';
+import Step2B from './UserTutorialImages/NewImages/Step2B.png';
+import Step3 from './UserTutorialImages/NewImages/Step3.png';
+import Step4 from './UserTutorialImages/NewImages/Step4.png';
+import Step3B from './UserTutorialImages/NewImages/Step3B.png';
 
 function UserTutorialPage() {
 
@@ -26,8 +31,7 @@ function UserTutorialPage() {
       {/*Step 1: Go to the Photo Upload Page*/}
       <div>
         <h2>Step 1: Go to the photo upload page</h2>
-        <p>This step navigates to the photo upload page</p>
-        <img src={photoUploadPageImage} alt="Photo Upload Page" />
+        <img src={Step1} alt="Photo Upload Page" />
       </div>
 
       {/*Step 2: Select what you would like to upload (shirts and pants)*/}
@@ -35,8 +39,7 @@ function UserTutorialPage() {
         <h2>Step 2: Select what you would like to upload (shirts or pants)</h2>
         <p>Select the item you would like to upload</p>
         <div>
-          <img src={shirtImage} alt="Shirt" />
-          <img src={pantsImage} alt="Pants" />
+          <img src={Step2B} alt="Shirt" />
         </div>
       </div>
 
@@ -44,7 +47,7 @@ function UserTutorialPage() {
       <div>
         <h2>Step 3: Align the camera with the outline of the item</h2>
         <p>Align your camera with the outline of the item on the screen</p>
-        <img src={frontPhotoImage} alt="Front Photo" />
+        <img src={Step3} alt="Front Photo" />
 
       </div>
 
@@ -52,42 +55,29 @@ function UserTutorialPage() {
       <div>
         <h2>Step 4: Confirm if the picture is taken good</h2>
         <p>Review the picture and confirm if it meets your expectations</p>
-        <img src={backPhotoImage} alt="Back Photo" />
+        <img src={Step3} alt="Back Photo" />
+        <img src={Step3B} alt="Back Photo" />
       </div>
 
       {/*Step 5: Turn around, align the outline with back side of the clothing item*/}
       <div>
         <h2>Step 5: Turn around, align outline with the back side of the clothing item</h2>
         <p>Turn around and align the outline with the back side of the clothing item</p>
-        <img src={backPhotoImage} alt="Back Photo" />
+        <img src={Step4} alt="Back Photo" />
       </div>
 
-      {/*Step 6: Take the picture of the backside*/}
+      {/*Step 6: Confirm if the back picture is good*/}
       <div>
-        <h2>Step 6: Take picture of the backside</h2>
-        <p>Take a picture of the back side of the clothing item</p>
-        <img src={backPhotoImage} alt="Back Photo" />
-      </div>
-
-      {/*Step 7: Confirm if the back picture is good*/}
-      <div>
-        <h2>Step 7: Confirm if back picture is good</h2>
+        <h2>Step 6: Confirm if back picture is good</h2>
         <p>Review the picture and confirm if it meets your expectations</p>
-        <img src={backPhotoImage} alt="Back Photo" />
+        <img src={Step4} alt="Back Photo" />
+        <img src={Step3B} alt="Back Photo" />
       </div>
 
-      {/*Step 8: Click Segment*/}
+      {/*Step 7: Wait a few minutes to see your 3D clothing item*/}
       <div>
-        <h2>Step 8: Click Segment</h2>
-        <p>Clock on the segment to proceed</p>
-        <img src={SegmentImage} alt="Segment" />
-      </div>
-
-      {/*Step 9: Wait a few minutes to see your 3D clothing item*/}
-      <div>
-        <h2>Step 9: Wait a few minutes to see your 3D clothing items</h2>
-        <p>Wait for a few minutes to see your result</p>
-        <p>Display the 3D clothing item here</p>
+        <h2>Step 7: Wait for Segmentation</h2>
+        <p>Check out your clothing item in the Model Viewer Page!</p>
       </div>
     </div>
   );
