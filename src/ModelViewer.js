90197a3415809f034f5af690533b81e0aba1e5c4
Joy Son <98338109+joyson0914@users.noreply.github.com>
2024-04-25 20:41:56 -0400

    Update ModelViewer.js

--- src/ModelViewer.js
+++ src/ModelViewer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useRef, useEffect } from 'react'; 
+import styles from './HomePage.module.css';
 
 // START EXAMPLE CODE
 import '@kitware/vtk.js/favicon';
@@ -210,10 +211,16 @@ function VTK() {
     return (
       <div>
         <h1>Model Viewer</h1>
-        <p>This is the Model Viewer.</p>
-        
-        <div style={{ zIndex: 99 }}>
-        <button onClick={toggleShowVisible}>Toggle show</button>{" "}
+        <p>You can view all clothing models here.</p>
+        <div style={{ zIndex: 99 , display: "flex",
+    justifyContent: "center"}}>
+          <button onClick={toggleShowVisible}
+            style={{
+              backgroundColor: "#5b5655",
+              color: "white",
+              fontSize: "16px",
+              padding: "10px 20px",
+    }}>Switch</button>{" "}
       </div>
       <div> {isVTKVisible ? <VTK /> : <div />} </div>
       </div>
@@ -223,25 +230,8 @@ function VTK() {
     );
     
   }
-/*     /*
-        <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Clothing Type</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={clothingType}
-            onChange={handleChange}
-          >
-            <MenuItem value={"shirt"}>Shirt</MenuItem>
-            <MenuItem value={"dress_shirt"}>Dress Shirt</MenuItem>
-            <MenuItem value={"pants"}>Pants</MenuItem>
-          </Select>
-        </FormControl>
-        </Box>
-        */
+
 
 //<div id="mainViewer" class="ui-widget-content" float= "right" width= "800px" max-height= "10%" background-color= "#000" boarder = "none" margin= "0 5px"> </div>
 
 export default ModelViewer;
-
